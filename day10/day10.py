f15a8a63db62172920d6ad5736cc9f99d2437c0b
DiogoAlves002 <diogoasilva@ua.pt>
2023-12-24 01:03:56 +0000

    day10 part 1

--- day10/day10.py
+++ day10/day10.py
@@ -2,8 +2,8 @@ from enum import Enum
 
 
 class Directions(Enum):
-	NORTH = (1, 0)
-	SOUTH = (-1, 0)
+	NORTH = (-1, 0)
+	SOUTH = (1, 0)
 	WEST = (0, -1)
 	EAST = (0, 1)
 
@@ -11,8 +11,8 @@ class Directions(Enum):
 
 def main():
 	filename = "input.txt"
-	filename = "test_2.txt"
-	filename = "test_1.txt"
+	#filename = "test_2.txt"
+	#filename = "test_1.txt"
 
 	STARTING_CHAR_TYPE = "S"
 	grid = {} # {col_y : {row_x: char}}
@@ -25,6 +25,13 @@ def main():
 		"J" : [Directions.NORTH, Directions.WEST],
 		"7" : [Directions.SOUTH, Directions.WEST],
 		"F" : [Directions.SOUTH, Directions.EAST],
+	}
+
+	opposite_direction = {
+		Directions.NORTH : Directions.SOUTH,
+		Directions.SOUTH : Directions.NORTH,
+		Directions.EAST : Directions.WEST,
+		Directions.WEST : Directions.EAST
 	}
 	
 
@@ -40,38 +47,80 @@ def main():
 				starting_pos = (row, col)
 		grid[row] = new_row
 
-	printGrid(grid)
-	print("\nstart:", starting_pos)
+	#printGrid(grid)
+	#print("\nstart:", starting_pos)
 
-	# TODO follow path until it leads back to start
+	biggest_step = 0
+	
 
+	# follow path until it leads back to start
 	possible_paths = getPipesNeighboursOfStart(starting_pos, pipe_connections, grid)
 
 	# NOTE maybe go one step in each path at a time
 	#      but beware that doing that would follow the correct path twice
 	for pipe_and_direction in possible_paths:
-		visited = set()
-		visited.add(starting_pos)
+		step = 0
+		path = set()
+		path.add(starting_pos)
+		
 
-		pipe, next_direction = pipe_and_direction
+		pipe_pos, next_direction = pipe_and_direction
 
-		while pipe != starting_pos and pipe not in visited:
-			visited.add(pipe)
-			pipe = getPosFromDirection(pipe, next_direction)
-			# TODO update  next_direction
+		while True:
+			step += 1
+			path.add(pipe_pos)
+
+			pipe_pos = getNextPipe(pipe_pos, next_direction)
+			if pipe_pos == starting_pos:
+				biggest_step = int(len(path) / 2)
+				break
+			if is_point(pipe_pos, grid):
+				break
+			next_direction = getNextDirection(pipe_pos, next_direction, opposite_direction, pipe_connections, grid)
+		
+		if biggest_step != 0:
+			break
+
+	print("part1:", biggest_step)
 
 
 
-def getPosFromDirection(curr, direction):
+def getNextPipe(curr, direction):
 	return (curr[0] + direction.value[0], curr[1] + direction.value[1])
 
+def is_point(pipe_pos, grid):
+	y, x = pipe_pos
+
+	pipe = grid[y][x]
+
+	return pipe == "."
 
 
+def getNextDirection(pos, direction, opposite_direction, pipe_connections, grid):
+	y, x = pos
+	pipe = grid[y][x]
+
+	pipe_directions = pipe_connections[pipe][:]
+	direction_we_came_from = opposite_direction[direction]
+	pipe_directions.remove(direction_we_came_from)
+
+	next_direction = pipe_directions[0]
+
+	return next_direction
 
 
-def getNextPipe(pos, direction_from, grid):
-	pass
+def getNeighbourNextDirection(candidate, pipe_connections):
+	if candidate == ".":
+		return None
 
+	candidate_directions = pipe_connections[candidate][:]
+
+	if Directions.SOUTH in candidate_directions:
+		candidate_directions.remove(Directions.SOUTH)
+		next_direction = candidate_directions[0]
+		return next_direction
+
+	return None
 
 
 
@@ -83,48 +132,37 @@ def getPipesNeighboursOfStart(pos, pipe_connections, grid):
 
 	if y > 0:
 		candidate = grid[y - 1][x] # up
+		
+		next_direction = getNeighbourNextDirection(candidate, pipe_connections)
 
-		candidate_directions = pipe_connections[candidate]
-
-		if Directions.SOUTH in candidate_directions:
-			candidate_directions.remove(Directions.SOUTH)
-			next_direction = candidate_directions[0]
-
+		if next_direction != None:
 			neighbours.append(((y - 1, x), next_direction))
 
 
 	if y < grid_last_y:
 		candidate = grid[y + 1][x] # down
 
-		candidate_directions = pipe_connections[candidate]
+		next_direction = getNeighbourNextDirection(candidate, pipe_connections)
 
-		if Directions.NORTH in candidate_directions:
-			candidate_directions.remove(Directions.NORTH)
-			next_direction = candidate_directions[0]
-
+		if next_direction != None:
 			neighbours.append(((y + 1, x), next_direction))
 
 
 	if x > 0:
 		candidate = grid[y][x - 1] # left
 
-		candidate_directions = pipe_connections[candidate]
+		next_direction = getNeighbourNextDirection(candidate, pipe_connections)
 
-		if Directions.WEST in candidate_directions:
-			candidate_directions.remove(Directions.WEST)
-			next_direction = candidate_directions[0]
-
+		if next_direction != None:
 			neighbours.append(((y, x - 1), next_direction))
+
 
 	if x < grid_last_x:
 		candidate = grid[y][x + 1] # right
 
-		candidate_directions = pipe_connections[candidate]
+		next_direction = getNeighbourNextDirection(candidate, pipe_connections)
 
-		if Directions.EAST in candidate_directions:
-			candidate_directions.remove(Directions.EAST)
-			next_direction = candidate_directions[0]
-			
+		if next_direction != None:
 			neighbours.append(((y, x + 1), next_direction))
 
 	return neighbours
